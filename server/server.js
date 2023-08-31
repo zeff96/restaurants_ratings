5c32da71a556b8d1ebb56851b4c3cb9101fbe569
zeff96 <zeffo1975@gmail.com>
2023-08-31 16:48:18 +0300

    Add method for editing and updating a restaurant on the server side

--- server/server.js
+++ server/server.js
@@ -15,8 +15,13 @@ app.get('/api/v1/restaurants/:id', (req, res) => {
 
 //create a given restaurant
 app.post('/api/v1/restaurants', (req, body) => {
-  
-})
+
+});
+
+//Update/edit a given restaurant
+app.put('/api/v1/restaurants/:id', (req, res) => {
+  res.json({ restaurants: { id: 1, name: 'Tatu Restaurant', location: 'Mkahawa', price_range: 3}})
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
