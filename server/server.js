401934e6390d0dbaaf8887ec435946f322a3c29e
zeff96 <zeffo1975@gmail.com>
2023-08-31 20:02:15 +0300

    Add server method for updating a restaurant

--- server/server.js
+++ server/server.js
@@ -62,9 +62,25 @@ app.post('/api/v1/restaurants', async(req, res) => {
 });
 
 //Update/edit a given restaurant
-app.put('/api/v1/restaurants/:id', (req, res) => {
-  console.log(req.params.id);
-  console.log(req.body)
+app.put('/api/v1/restaurants/:id', async(req, res) => {
+  const { id } = req.params;
+  const { name, location, priceRange } = req.body;
+  const query = {
+    text: 'UPDATE restaurants SET name = $1, location = $2, price_range = $3 WHERE restaurants.id = $4 RETURNING *',
+    values: [name, location, priceRange, id]
+  }
+  try {
+    const result = await db.query(query);
+    res.status(200).json({
+      status: 'success',
+      result: result.rowCount,
+      data: {
+        restaurant: result.rows[0]
+      }
+    });
+  } catch (error) {
+    res.status(500).json({ detail: error.detail });
+  }
 });
 
 //Delete a given restaurant
