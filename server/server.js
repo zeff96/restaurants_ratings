f2c21518297910d8ef6d4755f13d09fdc4c46ec0
zeff96 <zeffo1975@gmail.com>
2023-08-31 16:39:40 +0300

    Add test hardcode for get all restaurants

--- server/server.js
+++ server/server.js
@@ -3,6 +3,11 @@ const app = express();
 
 const PORT = process.env.PORT ?? 8000
 
+//Retrive list of all restaurants
+app.get('/api/v1/restaurants', (req, res) => {
+  res.json({ restaurants: { name: 'Macdonalds', location: 'Miami', price_range: 3}})
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
