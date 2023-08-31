55471df51ea8a77b2fb243b132d0f2a13deb7c27
zeff96 <zeffo1975@gmail.com>
2023-08-31 16:33:04 +0300

    Add restaurants table schema

---
+++ server/schema.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE restaurants;
+
+CREATE TABLE restaurants(
+  id BIGSERIAL NOT NULL,
+  name VARCHAR(50) NOT NULL,
+  location VARCHAR(50) NOT NULL,
+  price_range INT NOT NULL CHECK(price_range >= 1 AND price_range <= 5),
+  PRIMARY KEY(id)
+);
