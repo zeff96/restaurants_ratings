f43a66c111896adf75ea829fac8102c96293d1f4
zeff96 <zeffo1975@gmail.com>
2023-09-01 05:22:29 +0300

    Add components for the project

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ function App() {
   return (
     <div className="container">
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni placeat id error eos! Nam error praesentium non quis perspiciatis illo consectetur commodi animi temporibus? Distinctio.</p>
+      <i class="fa-solid fa-star"></i>
     </div>
   );
 }
