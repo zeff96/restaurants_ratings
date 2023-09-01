9b279aaa64612e39ef4517d1514ccdc6525300de
zeff96 <zeffo1975@gmail.com>
2023-09-01 07:14:11 +0300

    Add context hook for global management

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,26 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/Homepage';
 import DetailsPage from './pages/DetailsPage';
 import UpdatePage from './pages/UpdatePage';
+import { RestaurantsContextProvider } from './context/RestaurantsContext';
 
 const App = () => {
   return (
-    <div className="container">
-      <Router>
-        <Routes>
-          <Route exact path='/' element={<HomePage />}/>
-          <Route exact path='/restaurants/:id' element={<DetailsPage />}/>
-          <Route exact path='/restaurants/:id/update' element={<UpdatePage />}/>
-        </Routes>
-      </Router>
-    </div>
+    <RestaurantsContextProvider>
+      <div className='container'>
+        <Router>
+          <Routes>
+            <Route exact path='/' element={<HomePage />} />
+            <Route exact path='/restaurants/:id' element={<DetailsPage />} />
+            <Route
+              exact
+              path='/restaurants/:id/update'
+              element={<UpdatePage />}
+            />
+          </Routes>
+        </Router>
+      </div>
+    </RestaurantsContextProvider>
   );
-}
+};
 
 export default App;
