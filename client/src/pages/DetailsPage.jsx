d43eaaca3c1cdcf2ef292e4695a138f802911621
zeff96 <zeffo1975@gmail.com>
2023-09-01 18:27:44 +0300

    Add title to details page

--- client/src/pages/DetailsPage.jsx
+++ client/src/pages/DetailsPage.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useEffect, useContext } from 'react';
+import { useParams } from 'react-router-dom';
+import Restaurants from '../apis/Restaurants';
+import { RestaurantsContext } from '../context/RestaurantsContext';
 
 const DetailsPage = () => {
+  const { selectedRestaurant, setSelectedRestaurant } =
+    useContext(RestaurantsContext);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await Restaurants.get(`/${id}`);
+        setSelectedRestaurant(response.data.restaurant);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div>
-      
+      <h1 className='fw-light display-1 text-center'>
+        {selectedRestaurant && selectedRestaurant.name}
+      </h1>
     </div>
-  )
-}
+  );
+};
 
-export default DetailsPage
+export default DetailsPage;
