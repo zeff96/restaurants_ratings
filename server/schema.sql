27e3bacebe5a72d642b91c020f87d2861373b1af
zeff96 <zeffo1975@gmail.com>
2023-09-02 06:40:11 +0300

    Add reviews table schemas

--- server/schema.sql
+++ server/schema.sql
@@ -7,3 +7,15 @@ CREATE TABLE restaurants(
   price_range INT NOT NULL CHECK(price_range >= 1 AND price_range <= 5),
   PRIMARY KEY(id)
 );
+
+CREATE TABLE reviews(
+  id BIGSERIAL NOT NULL,
+  restaurant_id INT NOT NULL,
+  name VARCHAR(50) NOT NULL,
+  review TEXT NOT NULL,
+  rating INT NOT NULL CHECK(rating >=1 AND rating <= 5),
+  PRIMARY KEY(id),
+  FOREIGN KEY(restaurant_id) REFERENCES restaurants(id)
+);
+
+CREATE INDEX restaurant_id_idx ON reviews(restaurant_id ASC);
